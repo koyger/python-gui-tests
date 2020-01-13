1fec749c72a178b5058c28453e100fdeb95bbddb
koyger <koygerov.work@gmail.com>
2020-01-13 12:51:18 +0300

    Added preconditions check for group deletion test. If there is one or no groups left, new group is added before deletion

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -1,8 +1,13 @@
 import random
+import datetime
 
 
 def test_delete_group(app):
     old_list = app.groups.get_group_list()
+    if len(old_list) <= 1:
+        group_to_add = "Additional group " + str(datetime.datetime.now().strftime("%m_%d_%H:%M:%S"))
+        app.groups.add_new_group(group_to_add)
+        old_list = app.groups.get_group_list()
     group_to_del = random.choice(old_list)
     app.groups.delete_group(group_to_del)
     new_list = app.groups.get_group_list()
